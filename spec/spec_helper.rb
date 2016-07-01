92b1b0bd8654cb008519e90cb7b785e551bc8d15
James Poole <james.poole@fasthosts.com>
2016-07-01 15:26:42 +0100

    Added rspec tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,12 +23,20 @@ RSpec.configure do |c|
   set :docker_image, @image.id
   set :docker_container_create_options, {
     'User'       => '100000',
-    'HostConfig' => {
-      'PortBindings' => { "#{LISTEN_PORT}/tcp" => [{ 'HostPort' => "#{LISTEN_PORT}" }]}
-    }
   }
 
   describe "tests" do
-    include_examples 'docker-ubuntu-16'
+    
+    describe file('/etc/lsb-release') do
+      it { should contain 'DISTRIB_RELEASE=16.04' }
+    end
+    
+    describe package('ruby-rspec') do
+      it { should be_installed }
+    end 
+    
+    describe package('git') do
+      it { should be_installed }
+    end  
   end
 end
